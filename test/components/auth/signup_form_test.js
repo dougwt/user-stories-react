3ee695681def74d77b6a0c0ee0aa4373a8398480
Doug Tyler <dougwt@gmail.com>
2017-02-23 22:10:26 -0800

    Add auth component tests

---
+++ test/components/auth/signup_form_test.js
@@ -0,0 +1,46 @@
+import { renderComponent , expect } from '../../test_helper';
+import SignupForm from '../../../src/components/auth/signup_form';
+
+describe('components/auth/SignupForm' , () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(SignupForm);
+  });
+
+  it('renders something', () => {
+    expect(component).to.exist;
+  });
+
+  it('has the correct class', () => {
+    expect(component).to.have.class('signup-form');
+  });
+
+  it('shows a form', () => {
+    expect(component.find('form')).to.exist;
+  });
+
+  it('shows a email field', () => {
+    expect(component.find('input[name=email]')).to.exist;
+  });
+
+  it('shows a password field', () => {
+    expect(component.find('input[name=password]')).to.exist;
+  });
+
+  it('shows a passwordConfirm field', () => {
+    expect(component.find('input[name=passwordConfirm]')).to.exist;
+  });
+
+  it('shows a name field', () => {
+    expect(component.find('input[name=name]')).to.exist;
+  });
+
+  it('shows a submit button', () => {
+    expect(component.find('button[type=submit]')).to.exist;
+  });
+
+  xit('shows a error message alert', () => {
+    expect(component.find('.alert')).to.exist;
+  });
+});
