d39f76428c5049f4c675d52979a632d555752670
Doug Tyler <dougwt@gmail.com>
2017-02-24 17:38:53 -0800

    Fix staging client bug caused by react-router

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // Used to determine which environment the app is currently running in.
 export const NODE_ENV = process.env.NODE_ENV || 'dev'
 
-export const API_URI_PREFIX = process.env.API_URI_PREFIX || 'http://localhost:3001'
+export const API_URI_PREFIX = process.env.REACT_APP_API_URI_PREFIX || 'http://localhost:3001'
